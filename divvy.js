d86cdcf98a3059a981cc12c0dda4e4aa5243509c
hollowdoor <hollowdoor99@gmail.com>
2014-11-26 22:12:36 -0600

    The divvy.createSender function now has contents.

--- divvy.js
+++ divvy.js
@@ -30,6 +30,27 @@ if(divvy.running === 'node'){
         return function(req, res, options){
             options = options || {};
             
+            var root = (options.root) ? options.root : '/',
+                basename = path.basename(req.url),
+                dirname = path.dirname(url.parse(req.url).pathname),
+                modulename = path.join(__dirname, thisname);
+        
+            if(thisname !== basename)
+                return false;
+            if(root !== dirname)
+                return false;
+            
+            var readstream = fs.createReadStream(modulename);
+            
+            res.setHeader('content-type', 'application/javascript');
+            readstream.pipe(res).on('error', function(e){
+                res.writeHead(500);
+                res.end('500 Internal server error.');
+                console.log(thisname+' javascript stream failed. 500 error was sent.');
+            });
+            
+            return true;
+                
         };
     };
     
